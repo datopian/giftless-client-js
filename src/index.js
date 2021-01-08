bd659b68906460e3392e63b5acb34ee76bf74011
steveoni <steohenoni2@gmail.com>
2021-01-08 13:28:36 +0100

    [index.js][s]: add upload method

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 import axios from 'axios'
-
+import { GitLfsBasicTransfer, GitLfsMultipartTransfer} from './transfer';
+import { getFileHash } from './utils';
+axios.defaults.adapter = require('axios/lib/adapters/http');
 class Client {
   /**
    * Initialize client with the required parameters
    * @param {*} lfs_server string
    * @param {*} auth_token string
-   * @param {*} transfer_adapters string[]
+   * @param {*} transferAdapters string[]
    */
-  constructor(lfs_server, auth_token=null, transfer_adapters=['multipart-basic','basic']) {
+  constructor(lfs_server, auth_token=null, transferAdapters=['multipart-basic','basic']) {
     this._url = lfs_server.replace(/\/+$/g,'');
     this._auth_token = auth_token;
-    this._transfer_adapters = transfer_adapters;
+    this._transferAdapters = transferAdapters;
     this.LFS_MIME_TYPE = 'application/vnd.git-lfs+json'; 
+    this._transferClass = {
+      'basic': GitLfsBasicTransfer,
+      'multipart-basic': GitLfsMultipartTransfer
+    }
   }
 
+  /**
+   * 
+   * @param {String} prefix 
+   * @param {String} operation 
+   * @param {Array of Object} objects 
+   * @param {String} ref 
+   * @param {String} transfers 
+   */
   async batch(prefix, operation, objects, ref=null, transfers=null) {
     const url = this._urlFor(null,prefix, 'objects', 'batch');
     if (!transfers) {
-        transfers = this.transfer_adapters;
+        transfers = this._transferAdapters;
     }
 
-    const payload = {'transfer': transfers,
-                     'operations': operation,
-                     'object': objects
+    const payload = {'transfers': transfers,
+                     'operation': operation,
+                     'objects': objects
                     };
-    if (ref) payload['ref'] = 'ref';
+    if (ref) payload['ref'] = ref;
 
     const headers = {
       'Content-type': this.LFS_MIME_TYPE,
@@ -32,7 +46,7 @@ class Client {
     };
 
     if (this._auth_token) {
-      headers['Authorization'] = `Bearer ${this._auth_token}`;
+      headers['Authorization'] = `Bearer ${this.auth_token}`;
     }
     const response = await axios({
       method: 'post',
@@ -64,6 +78,44 @@ class Client {
       str.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]));
     }
     return str.join("&");
+  }
+
+  /**
+   * Upload a file using Git LFS Server
+   * @param {File} file 
+   * @param {String} organizationId 
+   * @param {String} datasetId 
+   * @param {String} onProgress 
+   */
+  async upload(file, organizationId, datasetId, onProgress) {
+    let prefix = `${organizationId}/${datasetId}`;
+    let ref = {name: 'refs/heads/master' };
+    let objects = [{
+      oid: await getFileHash(file, 'sha256'),
+      size: file.size,
+    }]
+    let response = await this.batch(prefix, 'upload',objects,ref);
+
+    if (response.status !== 200) {
+      throw `'batch' request failed: ${response.status}`;
+    }
+
+    const negotiatedTransfer = response.data.transfer;
+    const objectSpec = response.data.objects[0];
+
+    if (!objectSpec.actions) {
+      return false;
+    }
+
+    const transferClass = this._transferClass[negotiatedTransfer];
+    if (!transferClass) {
+      throw `Unknown negotiated tranfer mode: ${negotiatedTransfer}`;
+    }
+    
+    const transferAdapter = new transferClass(objectSpec.actions, file, onProgress);
+    await transferAdapter.upload();
+
+    return true;
   }
 }
 
